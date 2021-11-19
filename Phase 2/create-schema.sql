043c5b8ea54f41f6b63f450b77eaab5e0cdc7c27
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-11-18 17:53:47 -0800

    populated tables

--- Phase 2/create-schema.sql
+++ Phase 2/create-schema.sql
@@ -37,48 +37,30 @@ create table ingredients (
     
 );
 
-insert into customer values('Bismarck', 'bb', 'Germany', 8, 15, 42000);
-insert into customer values('Iowa', 'bb', 'USA', 9, 16, 46000);
-insert into customer values('Kongo', 'bc', 'Japan', 8, 14, 32000);
-insert into customer values('North Carolina', 'bb', 'USA', 9, 16, 37000);
-insert into customer values('Renown', 'bc', 'Britain', 6, 15, 32000);
-insert into customer values('Revenge', 'bb', 'Britain', 8, 15, 29000);
-insert into customer values('Tennessee', 'bb', 'USA', 12, 14, 32000);
-insert into customer values('Yamato', 'bb', 'Japan', 9, 18, 65000);
+insert into customer values(1, 'Joe', 'joeNuts', 'joeNuts@gmail.com', 'joe');
+insert into customer values(2, 'John', 'hehe', 'hehe@gmail.com', 'john');
+insert into customer values(3, 'Bill', 'billybob', 'billybob@gmail.com', 'bill');
+insert into customer values(4, 'Jason', 'jaysun', 'jaysun@gmail.com', 'jason');
 
-insert into dish values('California', 'Tennessee', 1915);
-insert into dish values('Haruna', 'Kongo', 1915);
-insert into dish values('Hiei', 'Kongo', 1915);
-insert into dish values('Iowa', 'Iowa', 1933);
-insert into dish values('Kirishima', 'Kongo', 1915);
-insert into dish values('Kongo', 'Kongo', 1913);
-insert into dish values('Missouri', 'Iowa', 1935);
-insert into dish values('Musashi', 'Yamato', 1942);
-insert into dish values('New Jersey', 'Iowa', 1936);
-insert into dish values('North Carolina', 'North Carolina', 1941);
-insert into dish values('Ramillies', 'Revenge', 1917);
-insert into dish values('Renown', 'Renown', 1916);
-insert into dish values('Repulse', 'Renown', 1916);
-insert into dish values('Resolution', 'Revenge', 1916);
-insert into dish values('Revenge', 'Revenge', 1916);
-insert into dish values('Royal Oak', 'Revenge', 1916);
-insert into dish values('Royal Sovereign', 'Revenge', 1916);
-insert into dish values('Tennessee', 'Tennessee', 1915);
-insert into dish values('Washington', 'North Carolina', 1941);
-insert into dish values('Wisconsin', 'Iowa', 1940);
-insert into dish values('Yamato', 'Yamato', 1941);
+insert into establishment values(1, 'Hells Kitchen', '631 Jenner Dr.',5,'530-655-3245');
+insert into establishment values(2, 'Cordon Bleu', '221 Rodeo Dr.',5,'530-653-3221');
+insert into establishment values(3, 'Applebees', '633 Apple Rd.',2,'530-655-3245');
 
-insert into establishment values('Denmark Strait', '1941-05-24');
-insert into establishment values('Guadalcanal', '1942-11-15');
-insert into establishment values('North Cape', '1943-12-26');
-insert into establishment values('Suriago Strait', '1944-10-25');
+insert into rating values(1, 'Hells Kitchen', 'N/A')
+insert into rating values(2, 'Cordon Bleu', 'Steak was amazing')
+insert into rating values(3, 'Applebees', 'The food sucked')
 
-insert into outcomes values('California', 'Suriago Strait', 'ok');
-insert into outcomes values('Kirishima', 'Guadalcanal', 'sunk');
-insert into outcomes values('Resolution', 'Denmark Strait', 'ok');
-insert into outcomes values('Wisconsin', 'Guadalcanal', 'damaged');
-insert into outcomes values('Tennessee', 'Suriago Strait', 'ok');
-insert into outcomes values('Washington', 'Guadalcanal', 'ok');
-insert into outcomes values('New Jersey', 'Suriago Strait', 'ok');
-insert into outcomes values('Yamato', 'Suriago Strait', 'sunk');
-insert into outcomes values('Wisconsin', 'Suriago Strait', 'damaged');
+insert into dish values(1, 'Ceasar Salad', 1, 1);
+insert into dish values(2, 'Ceasar Salad', 2, 2);
+insert into dish values(3, 'Ceasar Salad', 3, 3);
+insert into dish values(4, 'Ceasar Salad', 4, 4);
+
+insert into recipe values(1, )
+insert into recipe values(1, )
+insert into recipe values(1, )
+insert into recipe values(1, )
+
+insert into ingredients values()
+insert into ingredients values()
+insert into ingredients values()
+insert into ingredients values()
