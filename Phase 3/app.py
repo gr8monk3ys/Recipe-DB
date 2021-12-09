78731d7c3a1f86e72ed6d93abe1c6d646b50ba4f
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-12-09 13:09:38 -0800

    completed routing for navbar

--- Phase 3/app.py
+++ Phase 3/app.py
@@ -1,17 +1,15 @@
 import json
 from flask import Flask, render_template, request, redirect, url_for
 
-api = 'https://api.poweredbyvendify.me'
-
 app = Flask(__name__)
 
 @app.route('/')
 def menu():
     return render_template('menu.html')
 
-@app.route(api + '/serial_management')
+@app.route('/serial_management')
 def serial_management():
-    return render_tamplate('serial_management.html')
+    return render_template('serial_management.html')
 
 @app.route('/item_management')
 def item_management():
